19a057b5ef74acbe86c77254c0028e81f9aa3662
Isaac Reji <isaac.reji@kalvium.community>
2025-02-03 15:17:27 +0530

    milestone26

--- backend/src/controllers/order.controller.js
+++ backend/src/controllers/order.controller.js
@@ -21,11 +21,29 @@ async function CreateOrder(req,res){
             shippingAddress:address,
             totalAmount:totalAmount
         })
-        return res.status(201).send({message:"successful",success:true})
+        return res.status(201).send({message:" Data successfully fetched",success:true,order})
     }catch(er){
         return res.status(500).send({message:er.message,success:false})
     }
 }
+
+async function getUserOrders(req,res){
+const UserId=req.UserId
+try{
+if (mongoose.Types.ObjectId.isValid(UserId)){
+    return res.status(400).send({message:"Invalid User Id",success:false})
+}
+const checkUser=await UserModel.findOne({_id:UserId})
+        if (!checkUser){
+            return res.status(400).send({message:'user not present please signup',success:false})
+        }
+        const orders=await UserModel.findOne({_id:UserId})
+        return res.status(200).send({ message: 'Data Successfully fetched', success: true, orders });
+}catch(er){
+    return res.status(500).send({message:er.message,success:true})
+}
+}
 module.exports={
-    CreateOrder
+    CreateOrder,
+    getUserOrders
 }
--- backend/src/routes/order.route.js
+++ backend/src/routes/order.route.js
@@ -1,6 +1,8 @@
 const express=require('express')
 const router=express.Router()
 const verifyToken=require("../middlewares/jwt-verify.js")
+const { CreateOrder, getUserOrders } = require('../controllers/order.controller.js')
 
-router.post("/confirm=order",verifyToken,)
+router.post("/confirm-order",verifyToken,CreateOrder)
+router.get("/get-user-order",verifyToken,getUserOrders)
 module.exports=router
