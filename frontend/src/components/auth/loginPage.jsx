d2e963a258c85e9e2270ead1091a20218c66c0d7
Isaac Reji <isaac.reji@kalvium.community>
2025-02-18 13:50:17 +0530

    milestone 31 completed

--- frontend/src/components/auth/loginPage.jsx
+++ frontend/src/components/auth/loginPage.jsx
@@ -1,40 +1,120 @@
-import React from 'react'
-import { Link, useNavigate } from 'react-router-dom'
-// import logo from '../../assets/logo.webp';
-import axios from "axios"
-
+import axios from 'axios';
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { setUserEmail } from '../../Redux/User/UserActions';
+import { setEmail } from '../../Redux/User/UsersSlice';
 function LoginPage() {
+  const dispatch = useDispatch();
+  const [credentials, setCreds] = useState({
+    email: '',
+    password: '',
+  });
   const navigate = useNavigate();
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    console.log(name, value);
+    setCreds({
+      ...credentials,
+      [name]: value,
+    });
+  };
+  const handleClickLogin = async (e) => {
+    // axios request to backend
+    e.preventDefault();
+    const response = await axios.post(
+      'http://localhost:8080/user/login',
+      credentials
+    );
+    console.log(credentials);
+    dispatch(setUserEmail(credentials.email));
 
-  const handleClickLogin=async()=>{
-    const res=await axios.post('http://localhost:8080/user/login');
-    localStorage.setItem('token',res.data.token)
-    console.log(res)
-    navigate('/')
-  }
-  return(
-  <div className="h-screen flex items-center justify-center">
-    
-    <form onSubmit={handleClickLogin} className="space-y-6">
-        <div><img className="mx-auto h-10 w-auto" src="../../assets/logo.webp" alt="Logo" /></div>
-              <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900">
-            Sign in to your account
-          </h2>
-        <div>
-        <label htmlFor="" className="block text-sm/6 font-medium text-gray-900">Please enter your email</label>
-        <input type="text" placeholder='abc@example.com' className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"/>
-        </div>
-        <div>
-            <label htmlFor="" className="block text-sm/6 font-medium text-gray-900">enter your password</label>
-            <input type="password" placeholder='*******' className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
-        </div>
-        <button type="submit"
-                className="flex w-full justify-center rounded-md bg-green-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-sm hover:bg-green-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">submit</button>
-                <p className='text-center'> Dont have an account?signup <Link to={"./signupPage"} /></p>
-    </form>
-  </div>
-  )
-  
+    localStorage.setItem('token', response.data.token);
+    // console.log(data);
+    navigate('/');
+  };
+
+  return (
+    <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+        <img
+          className="mx-auto h-10 w-auto"
+          src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=600"
+          alt="Your Company"
+        />
+        <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900">
+          Login in to your account
+        </h2>
+      </div>
+
+      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+        <form className="space-y-6" onSubmit={handleClickLogin}>
+          <div>
+            <label
+              htmlFor="email"
+              className="block text-sm/6 font-medium text-gray-900"
+            >
+              Email address
+            </label>
+            <div className="mt-2">
+              <input
+                type="email"
+                name="email"
+                id="email"
+                autoComplete="email"
+                required
+                value={credentials.email}
+                onChange={handleChange}
+                className=" text-black block w-full rounded-md bg-white px-3 py-1.5 text-base outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+              />
+            </div>
+          </div>
+
+          <div>
+            <div className="flex items-center justify-between">
+              <label
+                htmlFor="password"
+                className="block text-sm/6 font-medium text-gray-900"
+              >
+                Password
+              </label>
+              <div className="text-sm">
+                <a
+                  href="#"
+                  className="font-semibold text-indigo-600 hover:text-indigo-500"
+                >
+                  Forgot password?
+                </a>
+              </div>
+            </div>
+            <div className="mt-2">
+              <input
+                type="password"
+                name="password"
+                id="password"
+                autoComplete="current-password"
+                required
+                onChange={handleChange}
+                className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+              />
+            </div>
+          </div>
+
+          <div>
+            <button
+              type="submit"
+              className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              Sign in
+            </button>
+          </div>
+          <p className="text-center">
+            Don't have an account ? <Link to={'/signup'}>Sign up</Link>
+          </p>
+        </form>
+      </div>
+    </div>
+  );
 }
 
-export default LoginPage
+export default LoginPage;
